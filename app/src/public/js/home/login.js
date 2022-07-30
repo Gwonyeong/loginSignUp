9fea333efb710c46457f15675c93a25c61f539ff
gwonyeong <qwe980121@naver.com>
2022-07-30 13:32:23 +0900

    로그인버튼 클릭시 아디,비번 출력

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,7 +10,18 @@ loginBtn.addEventListener("click", login);
 function login(){
     const req = {
         id : id.value,
-        psword: pw.value,
+        pw: pw.value,
     }
     console.log(req)
+
+    fetch("/login", {
+        method : "POST",
+        headers : {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)//오브젝트를 문자열로 바꿔줌
+    }).then((res) => res.json())
+    .then((res => {
+        
+    }))  //fetch로 데이터를 보내주고 반환받은 데이터로 then사용
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,13 +1,48 @@
 "use strict";
-const hello = (req, res) => {
-    res.render("home/index")
-};
 
-const login =  (req,res) => {
-    res.render("home/login") 
+
+const output = {
+    hello: (req, res) => {
+        res.render("home/index")
+    },
+    
+    login :  (req,res) => {
+        res.render("home/login") 
+    },
+    
 }
 
+const users = {
+    id: ["gwon", "test"],
+    pw: ["1234", "1234"]
+}
+const process = {
+    login : (req, res) => {
+        
+        const id = req.body.id;
+        const pw = req.body.pw;
+        console.log(id,pw)
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.pw[idx] === pw){
+                return res.json({
+                    success: true
+                })
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg:"로그인 실패"
+        })
+    }
+}
+
+
+
 module.exports = {
-    hello, // hello : hello
-    login  // login : login
+    output,
+    process
+    // hello : hello
+     // login : login
 }
